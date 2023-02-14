521e5d277d651608ffbda1365ad01813e557477d
Krishna Neupane <kneupane@email.sc.edu>
2023-02-13 21:46:38 -0500

    Update cuts.cpp

--- src/cuts.cpp
+++ src/cuts.cpp
@@ -147,11 +147,11 @@ bool Cuts::IsPip(int i) {
 bool Cuts::IsProton(int i) {
   if (_data->gpart() <= i) return false;
   bool _proton = true;
-  _proton &= (_data->pid(i) == 0);
+  _proton &= (_data->pid(i) != 0);
   _proton &= (_data->charge(i) == POSITIVE);
   _proton &= (_data->sc_cnd_layer(i) == 3);
-  _proton &= (_data->p(i) > 0.5);
-  _proton &= (_data->sc_extras_dedx(i) > (-3.43 * (_data->p(i)) + 5.8));
+  // _proton &= (_data->p(i) > 0.5);
+  // _proton &= (_data->sc_extras_dedx(i) > (-3.43 * (_data->p(i)) + 5.8));
 
   //   _proton &= (_data->charge(i) == POSITIVE);
   _proton &= (_data->pid(i) == PROTON);
