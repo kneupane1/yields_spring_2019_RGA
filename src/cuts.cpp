baa28cbf71ab30d367f20a1f72ccf84e93d1d928
Krishna Neupane <kneupane@email.sc.edu>
2023-02-13 21:43:07 -0500

    Update cuts.cpp

--- src/cuts.cpp
+++ src/cuts.cpp
@@ -116,11 +116,11 @@ bool Cuts::FiducialCuts() {
 bool Cuts::IsPip(int i) {
   if (_data->gpart() <= i) return false;
   bool _pip = true;
-  _pip &= (_data->pid(i) == 0);
-  _pip &= (_data->sc_cnd_layer(i) ==3 );
-  // _pip &= ((_data->p(i) < 0.5) || (_data->sc_extras_dedx(i) < (-3.43 * (_data->p(i)) + 5.8)));
-  _pip &= (_data->charge(i) == POSITIVE);
-/*
+  // _pip &= (_data->pid(i) == 0);
+  // _pip &= (_data->sc_cnd_layer(i) ==3 );
+  // // _pip &= ((_data->p(i) < 0.5) || (_data->sc_extras_dedx(i) < (-3.43 * (_data->p(i)) + 5.8)));
+  // _pip &= (_data->charge(i) == POSITIVE);
+
   _pip &= (_data->charge(i) == POSITIVE);
   _pip &= (_data->pid(i) == PIP);
   _pip &= (abs(_dt->dt_Pi(i)) < 0.5 || abs(_dt->dt_ctof_Pi(i)) < 0.4);
@@ -147,11 +147,11 @@ bool Cuts::IsPip(int i) {
 bool Cuts::IsProton(int i) {
   if (_data->gpart() <= i) return false;
   bool _proton = true;
-  // _proton &= (_data->pid(i) == 0);
-  // _proton &= (_data->charge(i) == POSITIVE);
-  // _proton &= (_data->sc_cnd_layer(i) == 3);
-  // _proton &= (_data->p(i) > 0.5);
-  // _proton &= (_data->sc_extras_dedx(i) > (-3.43 * (_data->p(i)) + 5.8));
+  _proton &= (_data->pid(i) == 0);
+  _proton &= (_data->charge(i) == POSITIVE);
+  _proton &= (_data->sc_cnd_layer(i) == 3);
+  _proton &= (_data->p(i) > 0.5);
+  _proton &= (_data->sc_extras_dedx(i) > (-3.43 * (_data->p(i)) + 5.8));
 
   //   _proton &= (_data->charge(i) == POSITIVE);
   _proton &= (_data->pid(i) == PROTON);
